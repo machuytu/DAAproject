304278759b1c67fc03f8e454b359dfda73c3a58b
TRAI <nacari321@gmail.com>
2020-06-18 15:49:23 +0700

    done index in student admin

---
+++ Assets/js/xulyadmin.js
@@ -0,0 +1,10 @@
+﻿let setid = null;
+
+function getid(setid) {
+    setid = setid;
+    console.log(setid);
+}
+function edituser(setid) {
+    setid = setid;
+    window.location.href = "/Admin/sinhviens/Edit/"+setid;
+}
