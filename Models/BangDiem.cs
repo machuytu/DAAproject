87af437699a14e1d713f43d1521377703ce53a93
TQP0403 <fantasy.tqp@gmail.com>
2020-07-02 11:16:45 +0700

    Update v1

--- Models/BangDiem.cs
+++ Models/BangDiem.cs
@@ -11,6 +11,10 @@ namespace ProjectDAA1.Models
         public string tenmon { get; set; }
         public string mamon { get; set; }
         public Nullable<int> sotc { get; set; }
+        public Nullable<double> hsqt { get; set; }
+        public Nullable<double> hsth { get; set; }
+        public Nullable<double> hsgk { get; set; }
+        public Nullable<double> hsck { get; set; }
         public Nullable<double> diemqt { get; set; }
         public Nullable<double> diemth { get; set; }
         public Nullable<double> diemgk { get; set; }
