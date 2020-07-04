ddacd07e5e449580082b2f50c52150458552a67f
tu <machuytu@gmail.com>
2020-07-04 23:34:56 +0700

    Thong bao gv sv

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using ProjectDAA1.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -22,12 +24,33 @@ namespace ProjectDAA1.Controllers
             }
             else
             {
-                var listthongbaochung = db.thongbaos.Where(x => x.tag == "Thông Báo Chung")
+                var listthongbaochung = db.thongbaos.Where(x => x.tag == "THÔNG BÁO CHUNG")
                     .OrderByDescending(x => x.thoigiandang).Take(10).ToList();
                 ViewBag.thongBaoChung = listthongbaochung;
+                var listthongnghibu = db.thongbaos.Where(x => x.tag == "THÔNG BÁO NGHỈ, BÙ")
+                    .OrderByDescending(x => x.thoigiandang).Take(10).ToList();
+                ViewBag.listthongnghibu = listthongnghibu;
             }
             return View();
         }
+
+        public FileResult Download(string fileName)
+        {
+            var name = fileName.Split('/');
+            byte[] fileBytes = System.IO.File.ReadAllBytes(fileName);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, name[0]);
+        }
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            thongbao thongbao = await db.thongbaos.FindAsync(id);
+            if (thongbao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thongbao);
+        }
+
         [ChildActionOnly]
         public ActionResult Header()
         {
